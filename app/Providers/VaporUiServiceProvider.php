edb844f996e50ba771d2970328a307ca7d572c84
Joaquín Gamboa FIgueroa <joaquin.gamboaf@gmail.com>
2021-03-11 11:44:06 -0300

    fix test vapor ui

--- app/Providers/VaporUiServiceProvider.php
+++ app/Providers/VaporUiServiceProvider.php
@@ -27,9 +27,7 @@ class VaporUiServiceProvider extends ServiceProvider
     protected function gate()
     {
         Gate::define('viewVaporUI', function ($user = null) {
-            return in_array(optional($user)->email, [
-                'joaquin.gamboa@dsmgroup.cl',
-            ]);
+            return $user->isSuperAdmin();
         });
     }
 
